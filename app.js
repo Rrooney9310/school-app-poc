886673732b307335e9851f6e822d51a8ee642034
Rrooney9310 <Rebecca.Rooney@southlanarkshire.gov.uk>
2026-03-23 11:49:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function test() {
-  document.getElementById("output").innerText = "It works!";
+async function test() {
+  const response = await fetch("/.auth/me");
+  const data = await response.json();
+
+  if (data.clientPrincipal) {
+    document.getElementById("output").innerText =
+      "Logged in as: " + data.clientPrincipal.userDetails;
+  } else {
+    document.getElementById("output").innerText =
+      "Not logged in";
+  }
 }
